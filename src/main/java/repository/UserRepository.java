41d1d078fa326545efae70308708f46dce7a9f35
25023175-lgtm <25023175@vnu.edu.vn>
2026-04-08 00:32:01 +0700

    Remove old Spring Boot code

--- src/main/java/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.auction.auctionweb.repository;
-
-import com.auction.auctionweb.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUsername(String username);
-    User findByEmail(String email);
-}
